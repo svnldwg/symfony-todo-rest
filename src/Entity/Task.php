f2fcea62395533713ceac6bbe859ea8342da9533
svnldwg <s.ludwig.g@gmail.com>
2020-12-30 10:13:48 +0100

    sets up entities to persist todos with tasks

---
+++ src/Entity/Task.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TaskRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TaskRepository::class)
+ */
+class Task
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private int $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private string $name;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private ?string $description;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=ToDo::class, inversedBy="tasks")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private ?ToDo $todo;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getTodo(): ?ToDo
+    {
+        return $this->todo;
+    }
+
+    public function setTodo(?ToDo $todo): self
+    {
+        $this->todo = $todo;
+
+        return $this;
+    }
+}
--- src/Entity/ToDo.php
+++ src/Entity/ToDo.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\ToDoRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -28,9 +30,20 @@ class ToDo
      */
     private \DateTimeInterface $updatedAt;
 
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private ?string $description;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Task::class, mappedBy="todo", orphanRemoval=true)
+     */
+    private Collection $tasks;
+
     public function __construct(string $name)
     {
         $this->name = $name;
+        $this->tasks = new ArrayCollection();
     }
 
     public function getName(): string
@@ -50,6 +63,48 @@ class ToDo
     public function setUpdatedAt(): self
     {
         $this->updatedAt = new \DateTimeImmutable();
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Task[]
+     */
+    public function getTasks(): Collection
+    {
+        return $this->tasks;
+    }
+
+    public function addTask(Task $task): self
+    {
+        if (!$this->tasks->contains($task)) {
+            $this->tasks[] = $task;
+            $task->setTodo($this);
+        }
+
+        return $this;
+    }
+
+    public function removeTask(Task $task): self
+    {
+        if ($this->tasks->removeElement($task)) {
+            // set the owning side to null (unless already changed)
+            if ($task->getTodo() === $this) {
+                $task->setTodo(null);
+            }
+        }
 
         return $this;
     }
