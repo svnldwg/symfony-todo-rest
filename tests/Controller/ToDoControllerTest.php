2cfcd5f27e943f8187e6b44161a59c7909b7b457
svnldwg <s.ludwig.g@gmail.com>
2021-01-10 12:00:01 +0100

    migrate GET tests to phpunit

--- tests/Controller/ToDoControllerTest.php
+++ tests/Controller/ToDoControllerTest.php
@@ -26,19 +26,133 @@ class ToDoControllerTest extends WebTestCaseWithDatabase
         self::assertSame('http://localhost/api/todos/1', $response->headers->get('Location'));
         self::assertJson($response->getContent());
 
+        $timestamps = $this->assertTimestamps(json_decode($response->getContent(), true, 512, JSON_THROW_ON_ERROR));
+
+        $expectedJson = sprintf(
+            <<<'JSON'
+                    {
+                        "id": 1,
+                        "name": "some name",
+                        "description": "some description",
+                        "tasks": [
+                            {
+                                "name": "some task",
+                                "description": null
+                            }
+                        ],
+                        "updatedAt": "%s",
+                        "createdAt": "%s"
+                    }
+                JSON,
+            $timestamps['updatedAt'],
+            $timestamps['createdAt']
+        );
+        self::assertJsonStringEqualsJsonString($expectedJson, $response->getContent());
+    }
+
+    public function testGetSingleToDo(): void
+    {
+        $this->addFixture(ToDoLoader::class);
+
+        $this->client->request('GET', '/api/todos/2');
+
+        $response = $this->client->getResponse();
+        self::assertSame(Response::HTTP_OK, $response->getStatusCode());
+
         $responseArray = json_decode($response->getContent(), true, 512, JSON_THROW_ON_ERROR);
-        self::assertAtomDateTime($responseArray['updatedAt']);
-        self::assertAtomDateTime($responseArray['createdAt']);
-        self::assertJsonStringEqualsJsonString(json_encode([
-            'id'          => 1,
-            'name'        => 'some name',
-            'description' => 'some description',
-            'tasks'       => [
-                ['name' => 'some task', 'description' => null],
-            ],
-            'updatedAt' => $responseArray['updatedAt'],
-            'createdAt' => $responseArray['createdAt'],
-        ], JSON_THROW_ON_ERROR), $response->getContent());
+        $timestamps = $this->assertTimestamps($responseArray);
+
+        $expectedJson = sprintf(
+            <<<'JSON'
+                    {
+                        "id": 2,
+                        "name": "ToDo 2",
+                        "description": null,
+                        "tasks": [
+                            {
+                                "name": "Task 1 for ToDo 2",
+                                "description": "Task description"
+                            },
+                            {
+                                "name": "Task 2 for ToDo 2",
+                                "description": null
+                            }
+                        ],
+                        "updatedAt": "%s",
+                        "createdAt": "%s"
+                    }
+                JSON,
+            $timestamps['updatedAt'],
+            $timestamps['createdAt'],
+        );
+
+        static::assertJsonStringEqualsJsonString($expectedJson, $response->getContent());
+    }
+
+    public function testGetNonExistingToDo(): void
+    {
+        $this->client->request('GET', '/api/todos/1');
+
+        $response = $this->client->getResponse();
+        self::assertSame(Response::HTTP_NOT_FOUND, $response->getStatusCode());
+        self::assertEmpty($response->getContent());
+    }
+
+    public function testGetAllToDos(): void
+    {
+        $this->addFixture(ToDoLoader::class);
+
+        $this->client->request('GET', '/api/todos');
+
+        $response = $this->client->getResponse();
+
+        self::assertSame(Response::HTTP_OK, $response->getStatusCode());
+        self::assertSame('application/json', $response->headers->get('Content-Type'));
+        self::assertJson($response->getContent());
+
+        $responseArray = json_decode($response->getContent(), true, 512, JSON_THROW_ON_ERROR);
+        static::assertIsArray($responseArray);
+        static::assertCount(2, $responseArray);
+        $timestamps1 = $this->assertTimestamps($responseArray[0]);
+        $timestamps2 = $this->assertTimestamps($responseArray[1]);
+
+        $expectedJson = sprintf(
+            <<<'JSON'
+                    [
+                        {
+                            "id": 1,
+                            "name": "ToDo 1",
+                            "description": "Description 1",
+                            "tasks": [],
+                            "updatedAt": "%s",
+                            "createdAt": "%s"
+                        },
+                        {
+                            "id": 2,
+                            "name": "ToDo 2",
+                            "description": null,
+                            "tasks": [
+                                {
+                                    "name": "Task 1 for ToDo 2",
+                                    "description": "Task description"
+                                },
+                                {
+                                    "name": "Task 2 for ToDo 2",
+                                    "description": null
+                                }
+                            ],
+                            "updatedAt": "%s",
+                            "createdAt": "%s"
+                        }
+                    ]
+                JSON,
+            $timestamps1['updatedAt'],
+            $timestamps1['createdAt'],
+            $timestamps2['updatedAt'],
+            $timestamps2['createdAt'],
+        );
+
+        static::assertJsonStringEqualsJsonString($expectedJson, $response->getContent());
     }
 
     public function testEmptyListOfTodos(): void
@@ -97,6 +211,17 @@ class ToDoControllerTest extends WebTestCaseWithDatabase
             [Request::METHOD_DELETE, '/api/todos'],
             [Request::METHOD_PUT, '/api/todos'],
             [Request::METHOD_POST, '/api/todos/1'],
+        ];
+    }
+
+    private function assertTimestamps(array $toDo): array
+    {
+        self::assertAtomDateTime($toDo['updatedAt']);
+        self::assertAtomDateTime($toDo['createdAt']);
+
+        return [
+            'updatedAt' => $toDo['updatedAt'],
+            'createdAt' => $toDo['createdAt'],
         ];
     }
 }
--- tests/Fixtures/ToDoLoader.php
+++ tests/Fixtures/ToDoLoader.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Fixtures;
 
+use App\Entity\Task;
 use App\Entity\ToDo;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Persistence\ObjectManager;
@@ -13,8 +14,14 @@ class ToDoLoader implements FixtureInterface
         $toDo = new ToDo();
         $toDo->setName('ToDo 1')
             ->setDescription('Description 1');
-
         $manager->persist($toDo);
+
+        $toDo = new ToDo();
+        $toDo->setName('ToDo 2')
+            ->addTask((new Task())->setName('Task 1 for ToDo 2')->setDescription('Task description'))
+            ->addTask((new Task())->setName('Task 2 for ToDo 2'));
+        $manager->persist($toDo);
+
         $manager->flush();
     }
 }
